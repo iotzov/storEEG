51fbb2c7a65f11c1188854569de239df962fe1c2
iotzov <ivaniotzov@gmail.com>
2016-09-21 15:22:59 -0400

    Add dataSetItem class and fix formatting

--- serverUI.py
+++ serverUI.py
@@ -5,27 +5,43 @@ class dataSetEditForm(nps.ActionForm):
        Allows user to update data for existing sets or enter data for new sets
        Created and called by the mainMenu"""
 
-       def create(self):
-           self.value = None
-           self.vName = self.add(nps.TitleText, name = "Data Set Name")
-           self.vExperimenter = self.add(nps.TitleText, name = "Experimenter:")
-           self.vPublications = self.add(nps.TitleText, name = "Publications:")
-           self.vDate = self.add(nps.TitleDateCombo, name = "Date:")
-           self.vStimTypes = self.add(nps.TitleMultiSelect, max_height=4,
-                   value=[1,], name="Choose 1 or more stimulus types:",
-                   values = ["Visual", "Audio", "Video", "Other"])
-        def on_cancel(self):
-            self.parentApp.switchFormPrevious()
-        def on_ok(self): # This should edit/add the dataSet as appropriate
-            pass
+    def create(self):
+        self.value = None
+        self.vName = self.add(nps.TitleText, name = "Data Set Name")
+        self.vExperimenter = self.add(nps.TitleText, name = "Experimenter:")
+        self.vPublications = self.add(nps.TitleText, name = "Publications:")
+        self.vDate = self.add(nps.TitleDateCombo, name = "Date:")
+        self.vStimTypes = self.add(nps.TitleMultiSelect, max_height=4,
+            value=[1,], name="Choose 1 or more stimulus types:",
+            values = ["Visual", "Audio", "Video", "Other"])
+
+    def on_cancel(self):
+        self.parentApp.switchFormPrevious()
+
+    def on_ok(self): # This should edit/add the dataSet as appropriate
+        pass
+
+class dataSetItem(nps.MultiLineAction):
+    """Widget that is displayed in the add/edit screen. Corresponds to a single data set"""
+    def __init__(self, *args, **keywords):
+        super(dataSetItem, self).__init__(*args, **keywords)
+
+    def displayValues(self, data):
+        return data.getName()
+
+    def actionHighlighted(self, act_on_this, keypress):
+        self.parent.parentApp.getForm('EDITDATASET').value = act_on_this[0]
+        self.parent.parentApp.switchForm('EDITDATASET')
+
 
 class mainMenu(nps.ActionForm):
     """Houses the main menu that the user can use to move around the program"""
     def onStart(self):
+        pass
     # Needs work
 
 class setDisplay(nps.FormMutt):
-    MAIN_WIDGET_CLASS = dataList
+    MAIN_WIDGET_CLASS = dataSetItem
 
     def beforeEditing(self):
         self.updateDisplayed()
@@ -33,5 +49,3 @@ class setDisplay(nps.FormMutt):
     def updateDisplayed(self):
         self.wMain.values = self.parentApp.database.listAll()
         self.wMain.display()
-      
-    def
