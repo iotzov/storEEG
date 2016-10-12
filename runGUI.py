1163ebd3b5aed6fdda4d8ab21ce410cfe9779e94
iotzov <ivaniotzov@gmail.com>
2016-10-12 18:17:21 -0400

    Improvements to GUI and new data set page, fix windows breaking when closed and re-opened

--- runGUI.py
+++ runGUI.py
@@ -2,23 +2,38 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+#class displaySetPage(
 class gadroRep:
 
-    def OpenBrowseDataWindow(self, button):
+    def BrowseDataButtonClicked(self, button):
+        self.BrowseDataSets = self.builder.get_object('BrowseDataSets')
         self.BrowseDataSets.show_all()
-        self.BrowseDataSets.hide()
 
-    def onDeleteWindow(self, *args):
+    def onDeleteMain(self, *args):
         Gtk.main_quit(*args)
 
-    def HelpButtonPressed(self, button):
-        self.HelpScreen.show_all()
+    def onDeleteWindow(self, *args):
+        args[0].hide()
+        return True
 
-    def QuitButtonPressed(self, button):
+    # Below needs to be fleshed out
+    def SearchButtonClicked(self, button):
+        pass 
+
+    def HelpButtonClicked(self, button):
+        self.HelpScreen = self.builder.get_object('HelpScreen')
+        self.HelpScreen.show()
+
+    def QuitButtonClicked(self, button):
         Gtk.main_quit()
 
     def AddNewButtonClicked(self, button):
+        self.AddOrModifyDataSet = self.builder.get_object('AddOrModifyDataSet')
         self.AddOrModifyDataSet.show_all()
+
+    def testGetChildren(self, button):
+        for x in button.get_parent().get_children():
+            print(Gtk.Buildable.get_name(x))
 
     def __init__(self):
         self.builder = Gtk.Builder()
@@ -27,9 +42,6 @@ class gadroRep:
         self.builder.connect_signals(self)
 
         self.MainMenu = self.builder.get_object('MainMenu')
-        self.BrowseDataSets = self.builder.get_object('BrowseDataSets')
-        self.HelpScreen = self.builder.get_object('HelpScreen')
-        self.AddOrModifyDataSet = self.builder.get_object('AddOrModifyDataSet')
         self.MainMenu.show_all()
 
 if __name__ == "__main__":
