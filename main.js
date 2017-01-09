59dc856f94893d84398b9dad73ca16fbb8157312
Ivan <ivaniotzov@gmail.com>
2017-01-09 03:15:37 -0500

    slight updates

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 250, height: 200})
+  mainWindow = new BrowserWindow({width: 250, height: 240})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
