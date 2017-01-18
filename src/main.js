f9f817f8a6fb742a76dcfcfb8cd297ea4051947f
iotzov <ivaniotzov@gmail.com>
2017-01-18 17:14:08 -0500

    new main page and some reorganization

--- src/main.js
+++ src/main.js
@@ -13,11 +13,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 250, height: 240})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'html', 'mainPage.html'),
     protocol: 'file:',
     slashes: true
   }))
