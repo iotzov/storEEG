b7841faa20c89d9d096b282efb107fddd9dfe882
iotzov <ivaniotzov@gmail.com>
2017-01-20 16:22:52 -0500

    yuuuuge progress

--- src/deps/js/mainPage.js
+++ src/deps/js/mainPage.js
@@ -4,6 +4,24 @@ function exitProgram() {
 	ipcRenderer.send('exit-clicked');
 }
 
-function openHelpWindow() {
-	ipcRenderer.send('help-clicked');
+function hideAllSectionsAndDeselectButtons() {
+	$("#main-area > div").hide() // Hide all sections
+
+	$(".nav-pills > li").removeClass("active")
 }
+
+$(".nav-link").on('click', function (event) {
+	hideAllSectionsAndDeselectButtons()
+
+	$('#' + $(this).data('section')).show()
+	$(this).parent().addClass("active")
+});
+
+const links = document.querySelectorAll('link[rel="import"]')
+
+// Import and add each page to the DOM
+Array.prototype.forEach.call(links, function (link) {
+	let template = link.import.querySelector('.section-template')
+	let clone = document.importNode(template.content, true)
+	document.querySelector('#main-area').appendChild(clone)
+})
