dfd25415f604b3e58e82b670aca64eb7629e5fae
Ivan Iotzov <ivaniotzov@gmail.com>
2017-01-27 01:10:33 -0500

    data entry now working :D

--- src/deps/js/mainPage.js
+++ src/deps/js/mainPage.js
@@ -1,4 +1,16 @@
 const {ipcRenderer} = require('electron')
+const localforage = require('localforage')
+var currentStudy = null;
+
+const links = document.querySelectorAll('link[rel="import"]')
+// Import and add each page to the DOM
+Array.prototype.forEach.call(links, function (link) {
+	let template = link.import.querySelector('.section-template')
+	let clone = document.importNode(template.content, true)
+	document.querySelector('#main-area').appendChild(clone)
+})
+
+// Function definitions
 
 function exitProgram() {
 	ipcRenderer.send('exit-clicked');
@@ -19,14 +31,16 @@ const handleFormSubmit = event => {
 
 const formToJSON = elements => [].reduce.call(elements, (data, element) => {
 	if(isValidEntry(element)) {
-		data[element.id] = element.value;
+		data[element.name] = element.value;
 	}
 	return data;
 }, {});
 
 function isValidEntry(element) {
-	return element.id && element.value;
+	return element.name && element.value;
 };
+
+// Event Handlers
 
 $(".data-entry").on('submit', handleFormSubmit);
 
@@ -37,12 +51,32 @@ $(".nav-link").on('click', function (event) {
 	$(this).parent().addClass("active")
 });
 
-const links = document.querySelectorAll('link[rel="import"]')
-// Import and add each page to the DOM
-Array.prototype.forEach.call(links, function (link) {
-	let template = link.import.querySelector('.section-template')
-	let clone = document.importNode(template.content, true)
-	document.querySelector('#main-area').appendChild(clone)
-})
+$("#initial-add-form").on('submit', function (event) {
+	event.preventDefault();
+
+	var data = formToJSON(event.currentTarget.elements);
+	localforage.getItem(data.studyTitle, (err, study) => {
+		if(study) {
+			alert('Study already exists!');
+		}
+		else {
+			data.subjects = [];
+			data.stimuli = [];
+			data.recordingParameterSets = [];
+			data.recordings = [];
+			data.events = [];
+			data.publications = [];
+			data.experimenters = [];
+			data.license = [];
+			data.contacts = [];
+
+			localforage.setItem(data.studyTitle, data, (value) => {
+				$("#add-section").hide();
+				$("#main-add-section").show();
+			});
+			console.log(JSON.stringify(data, null, "  "));
+		}
+	});
+});
 
 $("#home-section").show()
