a5d20b03e0697fb6b0300b07fbe7c3fa45cca786
Ivan Iotzov <ivaniotzov@gmail.com>
2018-03-13 23:11:47 -0400

    add basic processing script

--- deps/python/untitled0.py
+++ deps/python/untitled0.py
@@ -12,8 +12,9 @@ import matplotlib.pyplot as plt
 #import displayRaw
 #from types import MethodType
 import displayFunctions
+import json
 
-raw = mne.io.read_raw_edf('S18.bdf', stim_channel = -1, eog = range(64,70), preload=True)
+raw = mne.io.read_raw_edf('S23.bdf', stim_channel = -1, eog = range(64,70), preload=True)
 
 #raw.displayRaw = MethodType(displayRaw.displayRaw, raw)
 #raw.displayRaw()
@@ -27,3 +28,20 @@ channelsToDrop = [x.rstrip() for x in f.readlines()]
 f.close()
 
 raw.drop_channels(channelsToDrop)
+
+print(raw)
+
+with open('biosemi64_mapping.json','r') as x:
+    mapping = json.load(x)
+    x.close()
+
+raw.rename_channels(mapping)
+
+raw.set_montage(mne.channels.read_montage('biosemi64'))
+
+print(raw)
+print(raw.info)
+#raw.plot_sensors()
+
+raw.filter(0.5,None)
+raw.plot()
